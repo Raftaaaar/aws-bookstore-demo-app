fe5223f14c1eaa1ba376331a5a914660cc1545d8
Raftaaaar <kataria.yaksh@gmail.com>
2021-06-18 08:20:56 +1200

    Added 'chkComments.py' and implemented it's functionality in 'pipeline.sh'.

---
+++ assets/chkComments.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+import sys
+
+comment_match = '//Yaksh Kataria, 1330354'
+
+for file_path in Path('src').rglob('*.ts'):
+    with file_path.open() as curr_file:
+        if(curr_file.readline().strip() != comment_match.strip()):
+            error_out = str('### Comment not found at: ' + str(file_path))
+            print('0')
+            sys.exit(0)
+
+print('1')
+sys.exit(0)
--- assets/pipeline.sh
+++ assets/pipeline.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/env bash
 
-npm install
-npm run build
+echo $(py chkComments.py)
+if [ $(py chkComments.py) = 1 ] 
+then
+    echo '### All comments are present.'
+    exit 0
+else
+    echo '### Missing comments.'
+fi
+
+#npm install
+#npm run build
