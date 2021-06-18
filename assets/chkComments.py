bbae2281ccb1dd41a237bd92e6de9eae4507f10b
Raftaaaar <kataria.yaksh@gmail.com>
2021-06-18 20:33:44 +1200

    COMPX341-21A. Task 2 v2. Added documentation to code.

--- assets/chkComments.py
+++ assets/chkComments.py
@@ -1,14 +1,16 @@
+#Yaksh Kataria, 1330354
+
 from pathlib import Path
 import sys
 
-comment_match = '//Yaksh Kataria, 1330354'
+comment_match = '//Yaksh Kataria, 1330354'      #String to check.
 
-for file_path in Path('src').rglob('*.ts'):
-    with file_path.open() as curr_file:
-        if(curr_file.readline().strip() != comment_match.strip()):
-            error_out = str('### Comment not found at: ' + str(file_path))
-            print('0')
-            sys.exit(2)
+for file_path in Path('src').rglob('*.ts'):     #Iterate through each file in the 'src' folder with a filetype of '.ts'/
+    with file_path.open() as curr_file:     #Open the current file.
+        if(curr_file.readline().strip() != comment_match.strip()):      #If the first line of the current file is not the same as the string we are checking.
+            #error_out = str('### Comment not found at: ' + str(file_path))      #Variable for outputting an error, unused.
+            print('0')      #Print a '0' (in place of 'False') to idicate expected comments are not present.
+            sys.exit(2)     #Exit with an error-code.
 
-print('1')
-sys.exit(0)
+print('1')      #Print a '1' (in place of 'True') to idicate expected comments are present.
+sys.exit(0)     #Exit with a success-code.
--- assets/pipeline.sh
+++ assets/pipeline.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-echo $(py chkComments.py)
-if [ $(py chkComments.py) = 1 ] 
+#echo $(py chkComments.py)
+if [ $(py chkComments.py) = 1 ]     #Check to see if all expected comments are present.
 then
     echo '### All comments are present.'
 else
     echo '### Missing comments.'
-    exit 2
+    exit 2      #Exit with an error-code.
 fi
 
 npm install
